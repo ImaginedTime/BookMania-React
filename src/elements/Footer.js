cbca3884477d0ea3fab0e5680742adb331f95f49
Parth <psp90804@gmail.com>
2023-03-28 02:45:28 +0530

    added footer

--- src/elements/Footer.js
+++ src/elements/Footer.js
@@ -1,4 +1,19 @@
 
 function Footer() {
-    return 0;
+    return (
+        <footer>
+            <div class="socialmedia-links">
+                <a href="https:\\facebook.com\" class="fa fa-facebook"></a>
+                <a href="https:\\twitter.com\" class="fa fa-twitter"></a>
+                <a href="htpps:\\instagram.com\" class="fa fa-instagram"></a>
+            </div>
+            <div class="copyrights">
+                <span class="fa fa-copyright"> Copyrights 2023 | Book Magicia</span>
+            </div>
+            <div class="other-links">
+                <a href="">Terms and Conditions</a>
+                <a href="">Privacy Policy</a>
+            </div>
+        </footer>
+    );
 }
