5ab541aa0cf542347fe979c2e0bba615835b2676
Parth <psp90804@gmail.com>
2023-03-28 02:56:50 +0530

    class to className

--- src/elements/Footer.js
+++ src/elements/Footer.js
@@ -2,18 +2,20 @@
 function Footer() {
     return (
         <footer>
-            <div class="socialmedia-links">
-                <a href="https:\\facebook.com\" class="fa fa-facebook"></a>
-                <a href="https:\\twitter.com\" class="fa fa-twitter"></a>
-                <a href="htpps:\\instagram.com\" class="fa fa-instagram"></a>
+            <div className="socialmedia-links">
+                <a href="https:\\facebook.com\" className="fa fa-facebook"></a>
+                <a href="https:\\twitter.com\" className="fa fa-twitter"></a>
+                <a href="htpps:\\instagram.com\" className="fa fa-instagram"></a>
             </div>
-            <div class="copyrights">
-                <span class="fa fa-copyright"> Copyrights 2023 | Book Magicia</span>
+            <div className="copyrights">
+                <span className="fa fa-copyright"> Copyrights 2023 | Book Magicia</span>
             </div>
-            <div class="other-links">
+            <div className="other-links">
                 <a href="">Terms and Conditions</a>
                 <a href="">Privacy Policy</a>
             </div>
         </footer>
     );
 }
+
+export default Footer;
