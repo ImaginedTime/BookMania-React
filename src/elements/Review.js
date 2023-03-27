2a9f26dfc58e18f1cafbcf1e52b9ed21b5625758
Parth <psp90804@gmail.com>
2023-03-28 02:45:40 +0530

    added onclick function

--- src/elements/Review.js
+++ src/elements/Review.js
@@ -6,16 +6,32 @@ function ReviewSection({ reviews }) {
 
     const makedot = (id) => {
         return (
-        <span className="dot" onClick={() => setCurrentSlide(id)} key={`dot${id}`}></span>
+        <span className={`dot${(id === currentslide)?" activedot":""}`} onClick={() => setCurrentSlide(id)} key={`dot${id}`}></span>
         );
-    }
+    };
+
+    const changeSlide = (i) => {
+        let temp = currentSlide;
+        if ((temp + i) > reviews.length){
+            temp = 1;
+        }
+        else if ((temp + i) < 1){
+            temp = reviews.length;
+        }
+        else {
+            temp += i;
+        }
+        setCurrentSlide(temp);
+    };
 
     return (
         <div className="reviews-holder" id="Reviews">
             {reviews.map((review) => Review(review, currentSlide))}
             <i className="prev" onClick={() => changeSlide(-1)}>&#10094;</i>
             <i className="next" onClick={() => changeSlide(1)}>&#10095;</i>
-            <div className="dots">{reviews.map((review) => makedot(review.id))}</div>
+            <div className="dots">
+                {reviews.map((review) => makedot(review.id))}
+            </div>
         </div>
     );
 }
@@ -29,7 +45,9 @@ function Review(review, currentslide) {
 
     return (
         <div className="review" style={{display : ((id === currentslide) ? "grid" : "none")}} key={`review${id}`}>
-            <div className="review-img"><img className="review-img" src={imgsrc} /></div>
+            <div className="review-img">
+                <img className="review-img" src={imgsrc} />
+            </div>
             <div className="reviewer-email">
                 <h2>{email}</h2>
             </div>
