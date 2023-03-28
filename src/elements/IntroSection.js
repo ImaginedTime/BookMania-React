ad7fea00d0b6a67d0ae93c0b1b10a7e273ab04c9
Parth <psp90804@gmail.com>
2023-03-28 12:52:37 +0530

    added introSection

---
+++ src/elements/IntroSection.js
@@ -0,0 +1,21 @@
+
+function IntroSection() {
+    return (
+        <section id="Intro">
+            <div className="flex-container" id="Intro">
+                <div>
+                    <h1>
+                        <p>B</p>ook <p>M</p>agicia
+                    </h1>
+                    <p className="text">Welcome to prestigious bookstore where you can find all required books from adventure to
+                        science reference</p>
+                    <a href="#BestSellers" className="Bestsellers"><button className="Bestsellers">
+                        <p>B</p>est <p>S</p>ellers
+                    </button></a>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default IntroSection;
