0ec41fc29bd63a33ba6607d760f01c67fdedd45e
Parth <psp90804@gmail.com>
2023-03-29 03:24:45 +0530

    added keys

--- src/BookMania.js
+++ src/BookMania.js
@@ -5,9 +5,9 @@ import IntroSection from "./elements/IntroSection";
 import ReviewSection from "./elements/ReviewSection";
 import Footer from "./elements/Footer";
 
-import Bestsellers from "%PUBLIC_URL%/BestSellers.json";
-import Recommendations from "%PUBLIC_URL%/Recommendations.json";
-import Reviews from "%PUBLIC_URL%/Reviews.json";
+import Bestsellers from "./data/Bestsellers.json";
+import Recommendations from "./data/Recommendations.json";
+import Reviews from "./data/Reviews.json";
 
 
 function BookMania() {
@@ -18,19 +18,20 @@ function BookMania() {
 
     return (
         <>
+        <p>{bookCart}</p>
         <Header bookCart={bookCart} />
         <IntroSection />
         <section id="BestSellers">
             <div className="section-header"><b>Best Sellers</b></div>
             <div className="flex-container" id="BestSellers">
-                {bestsellerBooks.map((book) => <Card book={book} bookCart={bookCart} setBookCart={setBookCart} />)}
+                {bestsellerBooks.map((book) => <Card book={book} bookCart={bookCart} setBookCart={setBookCart} key={`BS${book.id}`} />)}
             </div>
         </section>
         <section id="Recommendations">
             <div className="section-header"><b>Recommendations</b></div>
             <div className="search-bar"><input type="text" placeholder="Search..." /><button>🔍</button></div>
             <div className="flex-container" id="Recommendations">
-                {recommendationBooks.map((book) => <Card book={book} bookCart={bookCart} setBookCart={setBookCart} />)}
+                {recommendationBooks.map((book) => <Card book={book} bookCart={bookCart} setBookCart={setBookCart} key={`RC${book.id}`} />)}
             </div>
         </section>
         <ReviewSection reviews={reviews} />
