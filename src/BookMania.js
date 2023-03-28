c0c5aca34d6707a4af17267b476a9456de918d14
Parth <psp90804@gmail.com>
2023-03-29 00:44:46 +0530

    added bookmania

--- src/BookMania.js
+++ src/BookMania.js
@@ -1,8 +1,33 @@
+import { useState } from "react";
+import Card from "./elements/Card";
+import Header from "./elements/Header";
+import IntroSection from "./elements/IntroSection";
+import ReviewSection from "./elements/ReviewSection";
 
 
 function BookMania() {
+    let [bookCart, setBookCart] = useState(Array(0));
+    let reviews, bestsellerBooks, recommendationBooks;
+
     return (
         <>
+        <Header bookCart={bookCart} />
+        <IntroSection />
+        <section id="BestSellers">
+            <div className="section-header"><b>Best Sellers</b></div>
+            <div className="flex-container" id="BestSellers">
+                {bestsellerBooks.map((book) => <Card book={book} bookCart={bookCart} setBookCart={setBookCart} />)}
+            </div>
+        </section>
+        <section id="Recommendations">
+            <div className="section-header"><b>Recommendations</b></div>
+            <div className="search-bar"><input type="text" placeholder="Search..." /><button>🔍</button></div>
+            <div className="flex-container" id="Recommendations">
+                {recommendationBooks.map((book) => <Card book={book} bookCart={bookCart} setBookCart={setBookCart} />)}
+            </div>
+        </section>
+        <ReviewSection reviews={reviews} />
+        <a href="#Intro" className="backtotop"><button className="backtotop">▲</button></a>
         </>
     );
 }
