76fa84c660222c356356d62c52f5b885eb79d1a9
Parth <psp90804@gmail.com>
2023-03-28 13:18:43 +0530

    stopped reload on submit

--- src/elements/ReviewSection.js
+++ src/elements/ReviewSection.js
@@ -11,7 +11,7 @@ function ReviewSection() {
                         <h1 className="review-form-title">Your Opinion <span>Matters</span>!</h1>
                         <textarea placeholder="Enter your reviews..." id="review-text"></textarea>
                         <input type="email" placeholder="Enter your email..." id="review-email" />
-                            <button id="review-submit">Submit</button>
+                            <button id="review-submit" onClick={(e) => {e.target.preventDefault()}}>Submit</button>
                     </form>
                 </div>
             </div>
